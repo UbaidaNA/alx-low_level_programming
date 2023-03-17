dcd87b312372a917344aecce5f784faf57606dd7
Ubaida Napari Abdulai <ubnapari@gmail.com>
2023-03-17 11:35:25 -0700

    Lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints alphabets in lowercase
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
