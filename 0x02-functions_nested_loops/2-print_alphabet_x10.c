7fc20b8d45737ea38d963a3e193a5132fa02275f
Ubaida Napari Abdulai <ubnapari@gmail.com>
2023-03-21 15:47:02 -0700

    10x alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,12 @@
 
 void print_alphabet_x10(void)
 {
-	char ch = 'a';
-	int i;
+	char ch;
+	int i = 0;
 
-	while (i < 11)
+	while (i <= 10)
 	{
+		ch = 'a';
 		while (ch <= 'z')
 		{
 			_putchar(ch);
