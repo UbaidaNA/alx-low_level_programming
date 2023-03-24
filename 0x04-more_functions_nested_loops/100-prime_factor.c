2ed5c81f5984cc7da60dd6bd450d8a7e16140ecd
Ubaida Napari Abdulai <ubnapari@gmail.com>
2023-03-24 06:19:35 -0700

    Largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - This program finds and prints the largest prime number in 612852475143
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int largest_factor = 0;
+	long int i;
+
+	while (n % 2 == 0)
+	{
+		largest_factor = 2;
+		n /= 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i +=2)
+	{
+		while (n % i == 0)
+		{
+			largest_factor = i;
+			n /= i;
+		}
+	}
+
+	if (n > 2)
+	{
+		largest_factor = n;
+	}
+
+	printf("%ld\n", largest_factor);
+
+	return (0);
+}
