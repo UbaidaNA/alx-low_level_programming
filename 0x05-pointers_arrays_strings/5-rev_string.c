8c3223549e4f4d3c8625667673bcfbffd8d89fa4
Ubaida Napari Abdulai <ubnapari@gmail.com>
2023-03-27 20:37:31 -0700

    Reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to string
+ * Return: 0 (Success)
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	char temp;
+
+	j = _strlen(s);
+
+	for (i = 0; i < j / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[j - 1 - i];
+		s[j - 1 - i] = temp;
+	}
+}	
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
