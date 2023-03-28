f02607987c1c825b1e8f8c46dfb3cbfb1bc35d4e
Ubaida Napari Abdulai <ubnapari@gmail.com>
2023-03-27 20:09:20 -0700

    Skip one char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of the string
+ * @str: pointer to the string
+ * Return: 0 (Success)
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
+
