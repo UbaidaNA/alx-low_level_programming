18d73ced967f2753a61bfea6485d1d418e0ab2f1
Ubaida Napari Abdulai <ubnapari@gmail.com>
2023-03-27 18:37:22 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of a pointer and the points it to 98
+ * @n: parameter to be updated
+ * Return: 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains C projects on pointers, arrays and strings
