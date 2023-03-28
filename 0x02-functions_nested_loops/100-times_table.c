89734275d748f3d41c7023a52b8a54ef53a4fd61
Ubaida Napari Abdulai <ubnapari@gmail.com>
2023-03-27 18:09:42 -0700

    Edited

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table
+ * @n: The timetable to be printed
+ * Return: Always 0
+ */
+
+
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This program prints fibonacci numbers
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+
+	for (i = 1; i <= 98; i++)
+	{
+		_putchar(a, '0');
+	
