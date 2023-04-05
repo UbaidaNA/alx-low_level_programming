968945aae061082b9bceaf3e99ad541c317cdaef
Ubaida Napari Abdulai <ubnapari@gmail.com>
2023-04-05 15:42:46 -0700

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the lenght of the string
+ * @s: string to be checked
+ * Return: string length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 +  _strlen_recursion(s + 1));
+	}
+}
