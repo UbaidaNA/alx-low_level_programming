0ac29f648131ca20bc6c83908d692bf445c2efc9
Ubaida Napari Abdulai <ubnapari@gmail.com>
2023-04-05 17:35:28 -0700

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,9 +8,14 @@
 
 int _sqrt_recursion(int n)
 {
+	int i = 1;
+	int mid;
+	int j = n;
+	int res;
+
 	if (n < 0)
 	{
-		return (0);
+		return (-1);
 	}
 	else if (n == 0 || n == 1)
 	{
@@ -18,5 +23,24 @@ int _sqrt_recursion(int n)
 	}
 	else
 	{
+		while (i <= j)
+		{
+			mid = (i + j) / 2;
 
-
+			if (mid * mid == n)
+			{
+				return (mid);
+			}
+			if (mid * mid < n)
+			{
+				i = mid + 1;
+				res = mid;
+			}
+			else
+			{
+				j = mid - 1;
+			}
+		}
+		return (res);
+	}
+}
