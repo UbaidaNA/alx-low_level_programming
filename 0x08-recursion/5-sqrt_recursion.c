ddbff37aef9b4f6b101cd82ea9b24cd24b681920
Ubaida Napari Abdulai <ubnapari@gmail.com>
2023-04-05 17:57:22 -0700

    recursive sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,46 +1,61 @@
 #include "main.h"
 
+
+/**
+ * _sqrt_help - reccursive helper function for _sqrt_recursion
+ * @n: integer whose sqrt is to be found
+ * @i: lowesr possible value of sqrt
+ * @j: highest possible value of sqrt
+ *
+ * Return: square root of n
+ * or -1 if no sqrt is found
+ */
+
+int _sqrt_help(int n, int i, int j);
+
+
 /**
  *  _sqrt_recursion - returns the natural square root of a number
  *  @n: integer whose square root is to be found
  *  Return: -1 if there is no sqaure root
+ *  or sqrt of n
  */
 
 int _sqrt_recursion(int n)
 {
-	int i = 1;
-	int mid;
-	int j = n;
-	int res;
 
 	if (n < 0)
 	{
 		return (-1);
 	}
-	else if (n == 0 || n == 1)
+	else
 	{
-		return (n);
+		return (_sqrt_help(n, 0, n));
+	}
+}
+
+
+int _sqrt_help(int n, int i, int j)
+{
+	int mid;
+
+	if (i > j)
+	{
+		return (-1);
+	}
+
+	mid = (i + j) / 2;
+
+	if (mid * mid == n)
+	{
+		return (mid);
+	}
+	else if (mid * mid > n)
+	{
+		return (_sqrt_help(n, i, mid - 1));
 	}
 	else
 	{
-		while (i <= j)
-		{
-			mid = (i + j) / 2;
-
-			if (mid * mid == n)
-			{
-				return (mid);
-			}
-			if (mid * mid < n)
-			{
-				i = mid + 1;
-				res = mid;
-			}
-			else
-			{
-				j = mid - 1;
-			}
-		}
-		return (res);
+		return (_sqrt_help(n, mid + 1, j));
 	}
 }
