fecdd716829206d10a05e48b6ec47a25a56f890e
Ubaida Napari Abdulai <ubnapari@gmail.com>
2023-04-10 18:06:39 -0700

    My Name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - This prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0  (Success)
+ */
+
+int main(int argc_attribute_((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
