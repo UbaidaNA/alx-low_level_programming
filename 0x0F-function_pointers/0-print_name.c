a9ec760c93af25c3ddfb41a18744b9a11f672234
Ubaida Napari Abdulai <ubnapari@gmail.com>
2023-04-19 14:50:29 -0700

    My Name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,9 +5,13 @@
  * print_name - prints a name
  * @name: name to be printed
  * @f: function to apply on name
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
+
 	f(name);
 }
